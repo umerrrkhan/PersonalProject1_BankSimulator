be3535044aca8f89865d41c058df03efa6fa0a10
Umer Yousuf Khan <104225484+Umeryousufkhan@users.noreply.github.com>
2023-10-23 19:04:47 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+"""This is a sample file. No code for the web application is written here"""
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
